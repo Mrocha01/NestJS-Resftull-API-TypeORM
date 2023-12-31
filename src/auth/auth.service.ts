e3db0fa6eb3fffe0e2b723bb2543e35bda453f35
MateusR <mateusftc_@hotmail.com>
2023-12-31 17:14:35 -0300

    feat(auth): Ajustes no controller e função guard.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,7 +38,7 @@ export class AuthService {
     };
   }
 
-  async checkToken(token: string) {
+  checkToken(token: string) {
     try {
       const data = this.jwtService.verify(token, {
         audience: this.audience,
@@ -51,7 +51,7 @@ export class AuthService {
     }
   }
 
-  async isValidToken(token: string) {
+  isValidToken(token: string) {
     try {
       this.checkToken(token);
       return true;

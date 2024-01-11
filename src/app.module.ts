ca08f1e59ffc3b6c6aeece9e97edb0a99f32275a
MateusR <mateusftc_@hotmail.com>
2024-01-10 21:51:54 -0300

    feat(app.module):  @nestjs-modules/mailer
    Config inicial usando o pug handlebars

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
@@ -13,6 +15,20 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     AuthModule,
     ThrottlerModule.forRoot([{ ttl: 60, limit: 100 }]),
+    MailerModule.forRoot({
+      transport:
+        'smtps://marcos.jaskolski@ethereal.email:RZE3KcrHB38x4gY9DP@smtp.ethereal.email',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],

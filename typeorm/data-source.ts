ff1597a31a067ee546e9fbf22ae8053c5272dfb8
MateusR <mateusftc_@hotmail.com>
2024-01-15 21:56:52 -0300

    feat(dataSource):Migrate:Down
    Adicionado também o diretório de migrações no projeto.

--- typeorm/data-source.ts
+++ typeorm/data-source.ts
@@ -10,6 +10,7 @@ const dataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  migrations: [`${__dirname}/migrations/**/*.ts`],
 });
 
 module.exports = dataSource;

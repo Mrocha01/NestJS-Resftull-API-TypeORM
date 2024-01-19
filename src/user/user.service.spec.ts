8b3e6553b58daebe1dbc53df4e971643a345e4ed
MateusR <mateusftc_@hotmail.com>
2024-01-18 23:39:44 -0300

    feat(T.UserService): Create finalizado

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { usersRepositoryMock } from '../testing/user-repository.mock';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { Role } from '../enums/role.enum';
+import { userEntityList } from '../testing/user-entity-list.mock';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -15,4 +18,23 @@ describe('UserService', () => {
   test('Validate definition', () => {
     expect(userService).toBeDefined();
   });
+
+  test('Method Create', async () => {
+    const data: CreateUserDTO = {
+      email: 'Rafael@hcode.com',
+      name: 'Rafael Antonio',
+      passwd: 'Rafael@123',
+      birthAt: new Date('1999-02-10'),
+      role: Role.Admin,
+    };
+    const result = await userService.create(data);
+
+    expect(result).toEqual(userEntityList[0]);
+  });
+
+  test('Method Read', async () => {});
+
+  test('Method Update', async () => {});
+
+  test('Method Delete', async () => {});
 });

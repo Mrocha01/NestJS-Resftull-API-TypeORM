490e7705103321b55eaaf7a9ef52fca29abd765f
MateusR <mateusftc_@hotmail.com>
2024-01-21 02:00:00 -0300

    feat(T.UserService): Update finalizado

---
+++ src/testing/update-user-dto.moc.ts
@@ -0,0 +1,5 @@
+import { UpdatePatchUserDTO } from '../user/dto/update-patch-user.dto';
+
+export const updatedUserDTO: UpdatePatchUserDTO = {
+  passwd: 'Rafael@456',
+};
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -7,10 +7,13 @@ import { createdUserDTO } from '../testing/create-user-dto.mock';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { updatedUserDTO } from '../testing/update-user-dto.moc';
 
 describe('UserService', () => {
   let userService: UserService;
   let userRepository: Repository<User>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService, usersRepositoryMock],
@@ -47,7 +50,13 @@ describe('UserService', () => {
     expect(result).toEqual(userEntityList[0]);
   });
 
-  test('Method Update', async () => {});
+  test('Method Update', async () => {
+    const data: UpdatePatchUserDTO = updatedUserDTO;
+
+    const result = await userService.update(1, data);
+
+    expect(result).toEqual(userEntityList[0]);
+  });
 
   test('Method Delete', async () => {});
 });

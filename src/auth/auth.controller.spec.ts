2ab72c6b6a88ec7d47efedd22f5182e764379865
MateusR <mateusftc_@hotmail.com>
2024-01-28 20:22:50 -0300

    feat(T.AuthContr): 100% os testes do controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -6,6 +6,11 @@ import { fileServiceMock } from '../testing/file-service.mock';
 import { authServiceMock } from '../testing/auth-service.mock';
 import { authLoginDTO } from '../testing/auth-login-dto.mock';
 import { acessToken } from '../testing/acess-token.mock';
+import { authRegisterDTO } from '../testing/auth-register-dto.mock';
+import { authForgetDTO } from '../testing/auth-forget-dto.mock';
+import { authResetDTO } from '../testing/auth-reset-dto.mock';
+import { userEntityList } from '../testing/user-entity-list.mock';
+import { getPhoto } from '../testing/get-photo.mock';
 
 describe('AuthController', () => {
   let authController: AuthController;
@@ -31,6 +36,39 @@ describe('AuthController', () => {
       const result = await authController.login(authLoginDTO);
 
       expect(result).toEqual({ acessToken: acessToken });
+    });
+
+    test('register method', async () => {
+      const result = await authController.register(authRegisterDTO);
+
+      expect(result).toEqual({ acessToken: acessToken });
+    });
+
+    test('forget method', async () => {
+      const result = await authController.forget(authForgetDTO);
+
+      expect(result).toEqual({ success: true });
+    });
+
+    test('reset method', async () => {
+      const result = await authController.reset(authResetDTO);
+
+      expect(result).toEqual({ acessToken: acessToken });
+    });
+  });
+
+  describe('Rotas autenticadas', () => {
+    test('me method', async () => {
+      const result = await authController.me(userEntityList[0]);
+
+      expect(result).toEqual(userEntityList[0]);
+    });
+
+    test('uploadPhoto method', async () => {
+      const photo = await getPhoto();
+      const result = await authController.uploadPhoto(userEntityList[0], photo);
+
+      expect(result).toEqual(photo);
     });
   });
 });

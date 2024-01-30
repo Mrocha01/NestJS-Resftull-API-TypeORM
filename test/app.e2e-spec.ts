c4972d7ffc64a1f7c7a4271b5165053dd2141e16
MateusR <mateusftc_@hotmail.com>
2024-01-29 23:22:30 -0300

    feat(e2e): Testes cobrindo as principais funções
    Vou verificar a necessidade de ampliar esses testes posteriormente.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { authRegisterDTO } from '../src/testing/auth-register-dto.mock';
+import { Role } from '../src/enums/role.enum';
+import dataSource from '../typeorm/data-source';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let acessToken: string;
+  let userId: number;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,5 +29,105 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('Registrar um novo usuario', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/register')
+      .send(authRegisterDTO);
+
+    expect(response.statusCode).toEqual(201);
+    expect(typeof response.body.acessToken).toEqual('string');
+  });
+
+  it('Logar um novo usuario', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: authRegisterDTO.email,
+        passwd: authRegisterDTO.passwd,
+      });
+
+    expect(response.statusCode).toEqual(201);
+    expect(typeof response.body.acessToken).toEqual('string');
+
+    acessToken = response.body.acessToken;
+  });
+
+  it('Obter os dados do usuario logado', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/me')
+      .set('Authorization', `bearer ${acessToken}`)
+      .send();
+
+    expect(response.statusCode).toEqual(201);
+    expect(typeof response.body.id).toEqual('number');
+    expect(response.body.role).toEqual(Role.User);
+  });
+
+  it('Registrar um novo usuario como admnistrador', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        ...authRegisterDTO,
+        role: Role.Admin,
+        email: 'henrique@teste.com.br',
+      });
+
+    expect(response.statusCode).toEqual(201);
+    expect(typeof response.body.acessToken).toEqual('string');
+
+    acessToken = response.body.acessToken;
+  });
+
+  it('Validando Role do usuario logado', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/me')
+      .set('Authorization', `bearer ${acessToken}`)
+      .send();
+
+    expect(response.statusCode).toEqual(201);
+    expect(typeof response.body.id).toEqual('number');
+    expect(response.body.role).toEqual(Role.User);
+
+    userId = response.body.id;
+  });
+
+  it('Usando funções de administrador', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/users')
+      .set('Authorization', `bearer ${acessToken}`)
+      .send();
+
+    expect(response.statusCode).toEqual(403);
+    expect(response.body.error).toEqual('Forbidden');
+  });
+
+  it('Alterando Role do usuario manualmente', async () => {
+    const ds = await dataSource.initialize();
+
+    const queryRunner = ds.createQueryRunner();
+
+    await queryRunner.query(
+      `UPDATE users SET role = ${Role.Admin} WHERE id = ${userId}`,
+    );
+
+    const rows = await queryRunner.query(`
+    SELECT * FROM users WHERE id = ${userId}`);
+
+    dataSource.destroy();
+
+    expect(rows.length).toEqual(1);
+    expect(rows[0].role).toEqual(Role.Admin);
+  });
+
+  it('Usando funções de administrador', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/users')
+      .set('Authorization', `bearer ${acessToken}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.length).toEqual(2);
   });
 });
